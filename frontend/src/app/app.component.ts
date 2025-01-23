8ce539923eb17119243e5940832ca0ebbc93cee8
dennisephraim <akainetteyephraim@gmail.com>
2025-01-23 11:40:51 -0500

    configured navbar and added routing

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,60 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ApiService } from './api.service';
-import { NavbarComponent } from './navbar/navbar.component';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {NgForOf, NgIf} from '@angular/common';
-import {forkJoin, Observable} from 'rxjs';
-import {map} from 'rxjs/operators'
+import { Component } from '@angular/core';
+import { RouterLinkActive, RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, ReactiveFormsModule, FormsModule, NgForOf, NgIf],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html'
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'frontend';
-  value: Array<{year: number, value: number}> = [
-    {year: 1 , value: 10000},
-    {year: 2 , value: 10000},
-    {year: 3 , value: 10000},
-    {year: 4 , value: 10000},
-    {year: 5 , value: 10000},
-    {year: 6 , value: 10000},
-    {year: 7 , value: 10000},
-    {year: 8 , value: 10000},
-    {year: 9 , value: 10000},
-    {year: 10 , value: 10000},
-  ]
-  mutualFunds: any = [];
-
-  ticker: string | null = null
-  initialInvestment: number | null = null
-  time: number | null = null
-
-  constructor(private apiService: ApiService) { }
-
-  ngOnInit() {
-    this.apiService.getMutualFund().subscribe((data: any) => {this.mutualFunds = data;})
-  }
-
-  calculateButtonClick() {
-    let newValues: Observable<{year: number, value: number}>[] = []
-    if (this.time && this.ticker && this.initialInvestment) {
-      for (let i = 1; i <= this.time; i++) newValues.push(
-        this.apiService.getFutureValue(this.ticker, this.initialInvestment, i).pipe(
-          map((data: any) => ({year: i, value: data}))))
-
-      forkJoin(newValues).subscribe((sortedValues: { year: number, value: number }[]) => {
-        sortedValues.sort((a, b) => {
-          return a.year - b.year;
-        });
-        this.value = sortedValues
-        console.log(sortedValues)
-      })
-    } else {
-      alert("All three fields are required!")
-    }
-  }
 }
---
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgForOf, NgIf} from '@angular/common';
+import {forkJoin, Observable} from 'rxjs';
+import {map} from 'rxjs/operators'
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [ ReactiveFormsModule, FormsModule, NgForOf, NgIf],
+  templateUrl: './dashboard.component.html',
+})
+
+export class DashboardComponent implements OnInit {
+    title = 'frontend';
+    value: Array<{year: number, value: number}> = [
+      {year: 1 , value: 10000},
+      {year: 2 , value: 10000},
+      {year: 3 , value: 10000},
+      {year: 4 , value: 10000},
+      {year: 5 , value: 10000},
+      {year: 6 , value: 10000},
+      {year: 7 , value: 10000},
+      {year: 8 , value: 10000},
+      {year: 9 , value: 10000},
+      {year: 10 , value: 10000},
+    ]
+    mutualFunds: any = [];
+  
+    ticker: string | null = null
+    initialInvestment: number | null = null
+    time: number | null = null
+  
+    constructor(private apiService: ApiService) { }
+  
+    ngOnInit() {
+      this.apiService.getMutualFund().subscribe((data: any) => {this.mutualFunds = data;})
+    }
+  
+    calculateButtonClick() {
+      let newValues: Observable<{year: number, value: number}>[] = []
+      if (this.time && this.ticker && this.initialInvestment) {
+        for (let i = 1; i <= this.time; i++) newValues.push(
+          this.apiService.getFutureValue(this.ticker, this.initialInvestment, i).pipe(
+            map((data: any) => ({year: i, value: data}))))
+  
+        forkJoin(newValues).subscribe((sortedValues: { year: number, value: number }[]) => {
+          sortedValues.sort((a, b) => {
+            return a.year - b.year;
+          });
+          this.value = sortedValues
+          console.log(sortedValues)
+        })
+      } else {
+        alert("All three fields are required!")
+      }
+    }
+  }
+
