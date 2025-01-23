6ca37bf4560467435fa7ef2320a82feaeaa23b37
dennisephraim <akainetteyephraim@gmail.com>
2025-01-23 11:54:07 -0500

    added icons and global font

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -4,6 +4,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {NgForOf, NgIf} from '@angular/common';
 import {forkJoin, Observable} from 'rxjs';
 import {map} from 'rxjs/operators'
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
@@ -26,24 +27,24 @@ export class DashboardComponent implements OnInit {
       {year: 10 , value: 10000},
     ]
     mutualFunds: any = [];
-  
+
     ticker: string | null = null
     initialInvestment: number | null = null
     time: number | null = null
-  
+
     constructor(private apiService: ApiService) { }
-  
+
     ngOnInit() {
       this.apiService.getMutualFund().subscribe((data: any) => {this.mutualFunds = data;})
     }
-  
+
     calculateButtonClick() {
       let newValues: Observable<{year: number, value: number}>[] = []
       if (this.time && this.ticker && this.initialInvestment) {
         for (let i = 1; i <= this.time; i++) newValues.push(
           this.apiService.getFutureValue(this.ticker, this.initialInvestment, i).pipe(
             map((data: any) => ({year: i, value: data}))))
-  
+
         forkJoin(newValues).subscribe((sortedValues: { year: number, value: number }[]) => {
           sortedValues.sort((a, b) => {
             return a.year - b.year;
